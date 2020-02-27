96923a2da231a61563a89b80cfa23794adb30312
EduardoReisDev <52722526+EduardoReisDev@users.noreply.github.com>
2020-02-27 13:35:32 -0300

    Initial commit

---
+++ Vagas/Vagas/Banco/Database.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using Vagas.Modelos;
+using Xamarin.Forms;
+
+namespace Vagas.Banco
+{
+    class Database
+    {
+        private SQLiteConnection _conexao;
+
+        public Database()
+        {
+            var dep = DependencyService.Get<ICaminho>();
+            string caminho = dep.ObterCaminho("database.sqlite");
+            _conexao = new SQLiteConnection(caminho);
+        }
+
+        public List<Vaga> Consultar()
+        {
+            return null;
+        }
+
+        public void Cadastro(Vaga vaga)
+        {
+            
+        }
+
+        public Vaga ObterVagaPorId(int id)
+        {
+            return null;
+        }
+
+        public void Atualizacao(Vaga vaga)
+        {
+            
+        }
+    }
+}
---
+++ Vagas/Vagas/Banco/ICaminho.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vagas.Banco
+{
+    interface ICaminho
+    {
+        string ObterCaminho(string NomeArquivoBanco);
+
+
+
+    }
+}
